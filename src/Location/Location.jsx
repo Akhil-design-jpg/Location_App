82e2e30616b5a3348ab17cc1f25078df92876b7f
Akhil-design-jpg <vakhil838@gmail.com>
2025-03-11 12:26:14 +0530

    map

--- src/Location/Location.jsx
+++ src/Location/Location.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
 
 
 const Location = () => {
@@ -7,20 +8,20 @@ const Location = () => {
     const [locate, setLocate] = useState(null)
 
 
-    useEffect(() => {
+  
         async function LocateVia() {
             try {
 
                 const loc = await new Promise((res, rej) => {
-                    navigator.geolocation.getCurrentPosition((position) =>{
+                    navigator.geolocation.getCurrentPosition((position) => {
 
                         const latitude = position.coords.latitude
                         const longitude = position.coords.longitude
                         res([latitude, longitude]),
-                        (error) =>
-                            rej(`Error occured in network ${error}`)
-                        
-                        
+                            (error) =>
+                                rej(`Error occured in network ${error}`)
+
+
                     }
                     )
 
@@ -33,10 +34,31 @@ const Location = () => {
             }
         }
         LocateVia()
-    }, [])
+  
     return (
         <div>
+            <button onClick={LocateVia}>
+                    get location 
+            </button>
             {locate ? <p>Latitude: {locate[0]}, Longitude: {locate[1]}</p> : <p>Loading...</p>}
+
+            {locate && (
+                <MapContainer center={locate} zoom={26} style={{ height: "480px", width: "480px" }} preferCanvas={true} >
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
+                        <Marker position={locate}>
+                <div style={{color: 'black'}}>
+                    located
+                            <Popup >Your location !!!</Popup>
+                </div>
+
+                        </Marker>
+
+                    </TileLayer>
+
+
+
+                </MapContainer>
+            )}
 
         </div>
     )
